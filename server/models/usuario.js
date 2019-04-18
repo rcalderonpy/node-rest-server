cad6e84a68aa38e5e505dcadeafd4d287dbffbbe
rcalderonpy <rcalderon@simons.com.py>
2019-04-18 14:58:13 -0400

    rest server - usuario

---
+++ server/models/usuario.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol válido'
+}
+
+let Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El correo es necesario']
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        required: [true, 'Se requiere establecer un role'],
+        enum: rolesValidos,
+        default: 'USER_ROLE'
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+
+});
+
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe ser único' });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
